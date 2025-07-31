48e8f329a75dd5b60739328669a0af4a10d082ea
GRock3 <guyadar100@gmail.com>
2025-07-31 15:42:46 +0300

    Organized the folders a bit and changed one point at the test in order to check polynomial after implementation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from coralearn.linear_model import LinearModel
+from coralearn.models.linear import LinearModel
 import numpy as np
 import matplotlib.pyplot as plt
 
 X = np.array([[1], [2], [3], [4]], dtype=np.float32)
-y = np.array([2, 4, 6, 8], dtype=np.float32)
+y = np.array([2, 8, 6, 8], dtype=np.float32)
 
 model = LinearModel(input_size=1)
 model.fit(X, y, epochs=1000, learning_rate=0.01)
@@ -13,4 +13,5 @@ print("Bias:", model.b)
 print("Prediction for 5:", model.predict(np.array([[5]], dtype=np.float32)))
 
 plt.plot(X,y,"x")
+plt.plot(X,model.predict(X))
 plt.show()
