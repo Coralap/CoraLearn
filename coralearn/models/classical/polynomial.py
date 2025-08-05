b5680a1dbd8f4a85094d1ad49ee322696711af6c
GRock3 <guyadar100@gmail.com>
2025-08-05 16:35:55 +0300

    added a robust and a minmax scaler

--- coralearn/models/classical/polynomial.py
+++ coralearn/models/classical/polynomial.py
@@ -1,6 +1,6 @@
 import numpy as np
 from coralearn.models.classical.basemodel import BaseModel
-from coralearn.scalers.Normalizers import zscore_normalize
+from coralearn.scalers.standard_scaler import StandardScaler
 
 class PolynomialModel(BaseModel):
     def __init__(self, input_size, degree=2):
@@ -22,16 +22,16 @@ class PolynomialModel(BaseModel):
         return poly_X
 
     def predict(self, X):
-        X_scaled, _, _ = zscore_normalize(X, self.X_mean, self.X_std)
+        X_scaled, _, _ = StandardScaler(X, self.X_mean, self.X_std)
         poly_X = self.expand_X(X_scaled)
         return poly_X.dot(self.w) + self.b
 
     def compute_gradient(self, X, y):
         # On first call during fit, compute and store mean/std
         if self.X_mean is None or self.X_std is None:
-            X_scaled, self.X_mean, self.X_std = zscore_normalize(X)
+            X_scaled, self.X_mean, self.X_std = StandardScaler(X)
         else:
-            X_scaled, _, _ = zscore_normalize(X, self.X_mean, self.X_std)
+            X_scaled, _, _ = StandardScaler(X, self.X_mean, self.X_std)
 
         X_expanded = self.expand_X(X_scaled)
         m = X_expanded.shape[0]
--- coralearn/scalers/Normalizers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def zscore_normalize(X, mean=None, std=None):
-    X = np.asarray(X)
-    if mean is None:
-        mean = X.mean(axis=0)
-    if std is None:
-        std = X.std(axis=0, ddof=0)
-        std[std == 0] = 1.0  # Avoid division by zero
-    return (X - mean) / std, mean, std
