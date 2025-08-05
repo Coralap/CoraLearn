01e837e2798613abc4d1edac1721d8530aac8e29
GRock3 <guyadar100@gmail.com>
2025-08-05 16:43:44 +0300

    added binary_cross_entropy loss

---
+++ coralearn/losses/binary_cross_entropy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def binary_cross_entropy(y_true, y_pred, eps=1e-15):
+    y_true = np.asarray(y_true)
+    y_pred = np.clip(np.asarray(y_pred), eps, 1 - eps)  # clip to avoid log(0)
+    bce = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    return bce
--- coralearn/losses/mse.py
+++ coralearn/losses/mse.py
@@ -1,3 +1,5 @@
 import numpy as np
 def mean_squared_error(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
     return np.mean((y_true - y_pred) ** 2)
