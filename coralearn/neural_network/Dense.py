3c2f8b2dd722d18a5054ddf766b48dddd3d48daf
GRock3 <guyadar100@gmail.com>
2025-08-07 14:42:10 +0300

    added a Dense.py file and a Sequential.py file with only forward prop and initialization

---
+++ coralearn/neural_network/Dense.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Dense():
+    def __init__(self, input_size, output_size, activation):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.activation = activation
+        self.W = np.random.randn(input_size, output_size) * 0.01  # small random weights
+        self.b = np.zeros((1, output_size))  # bias vector
+
+    def forward(self, A_in):
+        assert A_in.shape[1] == self.input_size, "Input size given does not match the layer's expected input size."
+        z = np.matmul(A_in, self.W) + self.b
+        a_out = self.activation(z)
+        return a_out
---
+++ coralearn/neural_network/Sequential.py
@@ -0,0 +1,8 @@
+class Sequential():
+    def __init__(self, layers):
+        self.layers = layers
+
+    def forward(self,X):
+        for layer in self.layers:
+            X = layer.forward(X)
+        return X
