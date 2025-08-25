3db1ee13d09c03493a5d9c458f73b485f6de2e05
GRock3 <guyadar100@gmail.com>
2025-08-25 19:41:29 +0300

    added sgd with momentum

--- tests/main.py
+++ tests/main.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 from coralearn.neural_network.Dense import Dense
 from coralearn.neural_network.Sequential import Sequential
 from coralearn.optimizers.SGD import SGD
+from coralearn.optimizers.SGDMomentum import SGDMomentum
 
 
 # Example binary classification data
@@ -64,7 +65,7 @@ def check_neural():
         Dense(input_size=2, output_size=3, activation=relu),
         Dense(input_size=3, output_size=1, activation=sigmoid)
     ])
-    optimizer = SGD(lr=0.05)
+    optimizer = SGDMomentum(lr=0.1)
     model.compile(mean_squared_error,optimizer=optimizer)
     model.train(X,y, epochs=1000)
     output = model.forward(X)
