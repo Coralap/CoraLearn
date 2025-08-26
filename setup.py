2678af698dcd434c6acd52e065c99f000cdda7c1
GRock3 <guyadar100@gmail.com>
2025-08-26 17:35:43 +0300

    changed the readme so it actually explains how it works

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="coralearn",
     version="0.1.8",
-    packages=find_packages(),
-    install_requires=[
-        "numpy>=1.23"  # any dependencies your library needs
-    ],
     author="Coralap",
-    description="An AI library written using only numpy.",
-    python_requires='>=3.8',
+    description="An AI library written using only NumPy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Coralap/CoraLearn",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_required=[
+        "numpy>=1.23",
+    ],
 )
